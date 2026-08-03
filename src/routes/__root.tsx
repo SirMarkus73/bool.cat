e976dbc744807794d1d3ddadad5eb69281b8419b
SirMarkus <marcosbarreroherrero@gmail.com>
2026-08-04 01:24:57 +0200

    feat: Mejorar la configuración de paraglide

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { Header } from "#/components/header";
+import { getLocale } from "#/paraglide/runtime";
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
@@ -30,7 +31,7 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang={getLocale()}>
 			<head>
 				<HeadContent />
 			</head>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,39 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js'
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }),
-    devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
+	resolve: { tsconfigPaths: true },
+	plugins: [
+		paraglideVitePlugin({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+			outputStructure: "message-modules",
+			cookieName: "PARAGLIDE_LOCALE",
+			strategy: ["url", "cookie", "preferredLanguage", "baseLocale"],
+			urlPatterns: [
+				{
+					pattern: "/:path(.*)?",
+					localized: [
+						["en", "/en/:path(.*)?"],
+						["es", "/es/:path(.*)?"],
+						["ca", "/ca/:path(.*)?"],
+					],
+				},
+			],
+		}),
+		devtools(),
+		nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+});
 
-export default config
+export default config;
