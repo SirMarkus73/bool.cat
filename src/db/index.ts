1a815e868d744a65a1ed0dc457a0bb4b6e52c05b
SirMarkus <marcosbarreroherrero@gmail.com>
2026-08-09 19:57:49 +0200

    fix: corregir formato y actualizar esquema en archivos de configuración

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { relations } from "./relations";
 
 export const db = drizzle({
+	// biome-ignore lint/style/noNonNullAssertion: We are sure that the DATABASE_URL environment variable is set, so we can safely use the non-null assertion operator here.
 	connection: process.env.DATABASE_URL!,
 	relations,
 });
